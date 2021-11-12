8a706082e56435e38aaeb41cb5f5bdf80a252b06
Aleksei Zhukov <alex__zhukov@mail.ru>
2021-11-12 12:31:30 +0300

    fix createPlayer function by video

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const scorpion = {
+const $arenas = document.querySelector('.arenas');
+
+const player1 = {
+    player: 1,
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -8,7 +11,8 @@ const scorpion = {
     }
 }
 
-const kitana = {
+const player2 = {
+    player: 2,
     name: 'Kitana',
     hp: 95,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
@@ -18,37 +22,43 @@ const kitana = {
     }
 }
 
-const $arenas = document.querySelector('.arenas');
 
-function createDivElement (element, classname) {
+
+function createElement (element, classname) {
     const $elem = document.createElement(element);
-    $elem.classList.add(classname);
+    if (classname) {
+        $elem.classList.add(classname);
+    }
+    
     return $elem;
 }
 
 
-function createPlayer (elem, classname, obj) {
+function createPlayer (objPlayer) {
     
-    elem.appendChild(createDivElement('div', classname));
-
-    const $player = document.querySelector(`div.${classname}`);
-    $player.appendChild(createDivElement('div',  `progressbar`));
-    $player.appendChild(createDivElement('div',  `character`));
-
-    const $progressbar =document.querySelector(`div.${classname} div.progressbar`);
-    $progressbar.appendChild(createDivElement('div', `life`));
-    $progressbar.appendChild(createDivElement('div', `name`));
-
-    const $life = document.querySelector(`div.${classname} div.life`);
-    $life.style.width = `${obj.hp}%`;
-
-    const $name = document.querySelector(`div.${classname} div.name`);
-    $name.innerText = obj.name;
-
+    const $player = createElement('div', `player${objPlayer.player}`);
+    const $progressbar = createElement('div', 'progressbar');
+    const $character = createElement('div', 'character');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
     const $img = document.createElement('img');
-    $img.src = obj.img;
-    document.querySelector(`div.${classname} div.character`).appendChild($img);
+
+    $life.style.width = `${objPlayer.hp}%`;
+    $name.innerText = objPlayer.name;
+    $img.src = objPlayer.img;
+
+    $progressbar.appendChild($name)
+    $progressbar.appendChild($life)
+
+    $character.appendChild($img)
+
+    $player.appendChild($progressbar)
+    $player.appendChild($character)
+    
+    return $player;
 
 }
-createPlayer($arenas, 'player1', scorpion)
-createPlayer($arenas, 'player2', kitana)
+
+$arenas.appendChild(createPlayer(player1))
+$arenas.appendChild(createPlayer(player2))
+
